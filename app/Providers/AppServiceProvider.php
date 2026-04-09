e58912217f9675fb85b77f8d33b2e356b2a7d0d6
raafi <raafistudent@gmail.com>
2026-04-09 20:01:45 +0700

    Initial commit: Mendefinisikan Hak Akses (Gate)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Gate untuk mengecek apakah user adalah Admin
+        Gate::define('is-admin', function (User $user) {
+            return $user->role === 'admin';
+        });
+
+        // Gate untuk mengecek apakah user adalah Owner
+        Gate::define('is-owner', function (User $user) {
+            return $user->role === 'owner';
+        });
+
+        // Gate untuk akses Dashboard (Admin & Owner)
+        Gate::define('view-reports', function (User $user) {
+            return in_array($user->role, ['admin', 'owner']);
+        });
     }
 }
