7f3328d132272e7320d7429f3973019b94d78a29
raafi <raafistudent@gmail.com>
2026-04-24 11:07:41 +0700

    Membuat SystemNotification untuk mengirim notifikasi berbasis database

--- app/Notifications/SystemNotification.php
+++ app/Notifications/SystemNotification.php
@@ -3,52 +3,37 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 class SystemNotification extends Notification
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct()
+    public $title;
+    public $message;
+    public $url;
+
+    // Menangkap data yang dikirim dari Controller
+    public function __construct($title, $message, $url)
     {
-        //
+        $this->title = $title;
+        $this->message = $message;
+        $this->url = $url;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
+    // Kita simpan notifikasinya di Database
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['database'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
-    {
-        return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
+    // Format data yang masuk ke database
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            'title' => $this->title,
+            'message' => $this->message,
+            'url' => $this->url,
         ];
     }
 }
