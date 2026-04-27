32fa6d3b058656044d1700840dca7e18e6ac0e79
raafi <raafistudent@gmail.com>
2026-04-27 14:52:34 +0700

    feast: merubah layout utama guest untuk menambahkan icon mata di password pada saat login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,6 +9,10 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800&display=swap" rel="stylesheet" />
+        
+        {{-- FITUR BARU: FontAwesome & AlpineJS (Wajib untuk Ikon Mata Show/Hide Password) --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
